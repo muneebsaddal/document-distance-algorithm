23463ce53526e4285aed8229404fa4203d69f977
muneeb <muneebsaddal@github.com>
2018-03-03 13:39:10 +0500

    more efficient way
    storing words and frequency into dictionary line by line instead of complete document. so it does not crash with large documents.

--- DocumentDistance.py
+++ DocumentDistance.py
@@ -1,54 +1,50 @@
-import re
 import math
 import time
 
 # timer starts here
 start_time = time.clock()
 
-# declaring dictionaries
-dic1 = {}
-dic2 = {}
+fhand = open('file 1.txt')
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
 
-# opening files
-D1 = open('file 1.txt', 'r') 	# 362,366 words
-D2 = open('file 2.txt', 'r') 	# 312,295 words
-
-# creating list of words
-str1 = D1.read().lower()
-str1 = re.findall(r'\b[a-z]{1,15}\b', str1)
-str2 = D2.read().lower()
-str2 = re.findall(r'\b[a-z]{1,15}\b', str2)
-
-# storing freqency of words in Dictionary
-for word in str1:
-	count = dic1.get(word, 0)
-	dic1[word] = count + 1
-
-for word in str2:
-	count = dic2.get(word, 0)
-	dic2[word] = count + 1
+fhand1 = open('file 2.txt')
+counts1 = dict()
+for line in fhand1:
+    words = line.split()
+    for word in words:
+        if word not in counts1:
+            counts1[word] = 1
+        else:
+            counts1[word] += 1
 
 # Calculating magnitude of document
 d1_magnitude = 0
-for key in dic1:
-	d1_magnitude += math.pow(dic1[key], 2)
+for key in counts:
+	d1_magnitude += math.pow(counts[key], 2)
 d1_magnitude = math.sqrt(d1_magnitude)
 
 d2_magnitude = 0
-for key in dic2:
-	d2_magnitude += math.pow(dic2[key], 2)
+for key in counts1:
+	d2_magnitude += math.pow(counts1[key], 2)
 d2_magnitude = math.sqrt(d2_magnitude)
 
 # Dot Product of common words
-d1_keys = set(dic1.keys())
-d2_keys = set(dic2.keys())
+d1_keys = set(counts.keys())
+d2_keys = set(counts1.keys())
 intersect_keys = d1_keys.intersection(d2_keys)
 a = []
 b = []
 c = []
 for letters in set(intersect_keys):
-	a.append(dic1[letters])
-	b.append(dic2[letters])
+	a.append(counts[letters])
+	b.append(counts1[letters])
 for value in range(len(a)):
 	c.append(a[value] * b[value])
 
